7dd8db4619b4efe447bf177a6d332a5444378298
Bruno Ellerbach <ingbrunofernandez@gmail.com>
2022-05-28 22:24:16 -0300

    Cliente del servicio

--- src/main/java/com/alura/gerenciador/servlet/EmpresasService.java
+++ src/main/java/com/alura/gerenciador/servlet/EmpresasService.java
@@ -22,18 +22,29 @@ public class EmpresasService extends HttpServlet {
 		
 		List<Empresa> empresas = new DB().getEmpresas();
 		
-		XStream xstream = new XStream();
-		xstream.alias("empresa", Empresa.class);
-		String xml = xstream.toXML(empresas);
+		String valor = request.getHeader("Accept");
 		
-		response.setContentType("Application/xml");
-		response.getWriter().print(xml);
+		if(valor.contains("xml")) {
+			XStream xstream = new XStream();
+			xstream.alias("empresa", Empresa.class);
+			String xml = xstream.toXML(empresas);
+			
+			response.setContentType("Application/xml");
+			response.getWriter().print(xml);
+		} else if(valor.contains("json")) {
+			Gson gson = new Gson();
+			String json = gson.toJson(empresas);
+			
+			response.setContentType("Application/json");
+			response.getWriter().print(json);
+		}else {
+			response.setContentType("Application/json");
+			response.getWriter().print("{'message':'No content'}");
+		}
 		
-//		Gson gson = new Gson();
-//		String json = gson.toJson(empresas);
-//		
-//		response.setContentType("Application/json");
-//		response.getWriter().print(json);
+		
+		
+
 		
 	}
 
